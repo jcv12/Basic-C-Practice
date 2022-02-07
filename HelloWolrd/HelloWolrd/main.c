dc8fcc2fea2d084f4afbbe5f87daf9f34babecf2
John Vayianos <john.c.vayianos@gmail.com>
2022-02-07 01:16:05 -0500

    first commit

---
+++ HelloWolrd/HelloWolrd/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  HelloWolrd
+//
+//  Created by John Vayianos on 2/4/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+//    char str[100];
+    int i;
+    
+    printf("Enter a value");
+    scanf("%d", &i);
+    
+    printf("\nYou entered: %d\n", i);
+    return 0;
+}
